3dc0e6810af2d732799c3761c872b01e14f0a443
REMEK <r.m.l.marciniak@gmail.com>
2020-03-05 01:10:35 +0100

    version 0.4

--- PythonProjectDNAComparisionAndVisualization/main.py
+++ PythonProjectDNAComparisionAndVisualization/main.py
@@ -1,3 +1,8 @@
+import urllib
+
+import requests
+
+
 def main():
     print("Hello in DNA comparision and visualization program.")
     print("\nSelect 1st sequence \n")
@@ -20,6 +25,72 @@ def main():
 
     # show dot plot
     show_dot_plot(data_array_first, description_first, data_array_second, description_second, dot_plot)
+
+    # filtering
+    window_size = input("Enter window size: ")
+
+    threshold = input("Enter threshold: ")
+
+    window_size = int(str(window_size))
+
+    threshold = int(str(threshold))
+
+    filtered_dot_plot = filter_dot_plot(window_size, threshold, dot_plot)
+
+    show_filtered_dot_plot(filtered_dot_plot)
+
+    # show matrix as a figure
+    # title,axes,legends,scale
+
+    # save matrix to a graphical file
+
+
+def show_filtered_dot_plot(filtered_dot_plot):
+    for i in range(len(filtered_dot_plot)):
+        for j in range(len(filtered_dot_plot[i])):
+            print(filtered_dot_plot[i][j], end=' ')
+        print()
+
+
+def filter_dot_plot(window_size, threshold, dot_plot):
+    filtered_dot_plot = dot_plot
+    window_x = 0
+    window_y = 0
+    window_x_range = window_size
+    window_y_range = window_size
+
+    for i in range(len(dot_plot) - window_size):
+        window_x += i
+        print(window_x)
+        window_x_range += i
+        print(window_x_range)
+        for j in range(len(dot_plot[i]) - window_size):
+            window_y += j
+            print(window_y)
+            window_y_range += j
+            print(window_y_range)
+
+            counter = 0
+
+            for window_x in range(window_x_range):
+                for window_y in range(window_y_range):
+                    if window_x == window_y:
+                        if dot_plot[window_x][window_y] == 1:
+                            counter += 1
+
+            if counter >= window_size - threshold:
+
+                for window_x in range(window_x_range):
+                    for window_y in range(window_y_range):
+                        if window_x == window_y:
+                            if dot_plot[window_x][window_y] == 1:
+                                filtered_dot_plot[i][j] = 1
+                            else:
+                                filtered_dot_plot[i][j] = 0
+                        else:
+                            filtered_dot_plot[i][j] = 0
+
+    return filtered_dot_plot
 
 
 def show_dot_plot(data_array_first, name_first, data_array_second, name_second, dot_plot):
@@ -106,12 +177,13 @@ def read_file():
         contents = file_object.read()
         return contents
 
-    # TO DO
-
 
 def read_file_from_web(url):
-    # lib requests
-    return url
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    text = data.decode('utf-8')
+    print(text)
+    return text
 
 
 def select_method():
@@ -135,7 +207,13 @@ def select_method():
             break
         elif label == "3":
             print("Selected 3rd option")
-            # soon
+            db = 'nuccore'
+            nuccore_id = input("Enter id:")
+            # covid 2019 id = 1798174254
+            url_address = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=%s&id=%s&rettype=fasta&retmode=text" % (
+                db, nuccore_id)
+            file = read_file_from_web(url_address)
+            return string_to_array(file)
             break
         else:
             print("Please select correct method ")
